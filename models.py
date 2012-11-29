ab7f9e10933e39379e64a00c3bb78261550633fb
Tyson Holmes <hectorscout@hectorscout.com>
2012-11-28 21:24:08 -0700

    actually changed view.py. Added Player to models

--- models.py
+++ models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
-class Settings(models.Model):
+class Setting(models.Model):
     name = models.CharField(max_length=32)
     value = models.CharField(max_length=64)
 
     def __unicode__(self):
-        return '%s:%s' % (self.name, self.value)
+        return '%s: %s' % (self.name, self.value)
+
+class Player(models.Model):
+    name = models.CharField(max_length=128)
+    score = models.IntegerField()
+
+    def __unicode__(self):
+        return '%s: %s' % (self.name, self.score)
